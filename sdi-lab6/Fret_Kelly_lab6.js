6cf5d955b8879e1396ce81771f327c78f8c0cbb7
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-16 17:12:01 -0400

    Commit 5 - Lab 6 - Array Project.

--- sdi-lab6/Fret_Kelly_lab6.js
+++ sdi-lab6/Fret_Kelly_lab6.js
@@ -13,23 +13,42 @@ var familyFunction = function() {
 	// literal array declaration with values
 	var nameArray = ["Cindy", "Rheanna", "Ness", "Darren", "Amethyst", "Daija", "Deziree", "Sugey"];
 	var birthdayArray = ["May 30th", "Nov 26th", "Dec 28th", "Nov 18th", "Nov 21st", "Feb 5th", "Jul 5th", "Aug 10th"];
+	console.log("Initial array values.\n");
+	console.log(nameArray);
+	console.log(birthdayArray);
+	console.log("\n");
+		
 	
-	
+	// array name and birthday output
 	for (var indexCounter = 0; indexCounter < nameArray.length; indexCounter++) {
-		console.log(nameArray[indexCounter] + "'s birthday is on " + birthdayArray[indexCounter] + ".\n");
+		console.log(nameArray[indexCounter] + "'s birthday is on " + birthdayArray[indexCounter] + ".");
+		
 	};
 	
 	
 	// array's reversed method
 	nameArray.reverse();
 	birthdayArray.reverse();
-	console.log("\nReversed names: \n" + nameArray + "\n\nReversed birthdays: \n" + birthdayArray + "\n");
+	console.log("\n");
+	console.log("\nReversed names:\n");
+	console.log(nameArray);
+	console.log("\nReversed birthdays:\n");
+	console.log(birthdayArray);
+	console.log("\n");
 	
 	
-	// array's join method
+	// array's shift method to remove the first array element
+	var removedName = nameArray.shift();
+	var removedBday = birthdayArray.shift();
+	console.log(removedName + "'s name and " + removedBday + " birthday was removed.\n");
+	console.log("\n");
+	
+	
+	// array's join method to join the array elements as a string
 	var theirName = nameArray.join("\n");
 	var theirBirthday = birthdayArray.join("\n");
-	console.log("\nJoined names:\n" + theirName + "\n\nJoined birthdays:\n" + theirBirthday);
+	console.log("\nJoined names after " + removedName + "'s name was removed.\n" + theirName + "\n");
+	console.log("\nJoined birthdays after " + removedBday + " birthday was removed.\n" + theirBirthday + "\n");
 		
 	
 	
